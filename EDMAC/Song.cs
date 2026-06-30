3146e8ce930c1e5cb51481b4331df20dad46afbb
Ben Sinnott <Ben.Sinnott@bridgeheadsoftware.com>
2026-06-30 19:43:56 +0100

    Fixed rounding error.

--- EDMAC/Song.cs
+++ EDMAC/Song.cs
@@ -39,7 +39,7 @@ public sealed class Song
             return null;
         }
 
-        int absoluteQuarter = (int)Math.Floor(samplePosition / SamplesPerChordQuarter);
+        int absoluteQuarter = (int)Math.Floor((samplePosition + 0.5) / SamplesPerChordQuarter);
         return progression.GetChordAtQuarter(absoluteQuarter);
     }
 
